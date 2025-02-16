187b9c863287d7ab27fac1767c68b5df43186116
Rachel <s1843940@ed.ac.uk>
2025-02-16 16:43:32 +0000

    intial push of DPDK on server-side

--- distmult_server.cc
+++ distmult_server.cc
@@ -2,11 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/select.h>
-#include <sys/epoll.h>
-#include <poll.h>
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -14,13 +9,18 @@
 #include <queue>
 #include <unordered_map>
 #include <vector>
-#include <fcntl.h>
-#include <sys/file.h>
-#include <csignal>
+
 #include "absl/flags/parse.h"
 #include "absl/log/globals.h"
 #include "absl/log/initialize.h"
 #include "absl/log/log.h"
+
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_mbuf.h>
+#include <rte_ether.h>
+#include <rte_ip.h>
+#include <rte_udp.h>
 
 #include "distmult_service.pb.h"
 #include "task_handler.h"
@@ -34,50 +34,54 @@ using matrixclass::MatrixClass;
 
 std::atomic<bool> running(true); // Flag to control the server loop
 
+
+#define RX_RING_SIZE 1024
+#define TX_RING_SIZE 1024
+#define NUM_MBUFS 8192
+#define MBUF_CACHE_SIZE 250
+#define BURST_SIZE 32
+
+uint16_t portid = 0;
+
 class DistMultServer
 {
 public:
     explicit DistMultServer(const std::string &ip_address, int port, TaskHandler *handler)
-        : task_handler_(handler), client_socket_(-1), stop_writer_thread_(false)
+        : task_handler_(handler), stop_reader_thread_(false), stop_writer_thread_(false)
     {
 
-        server_socket_ = socket(AF_INET, SOCK_STREAM, 0);
-        if (server_socket_ == -1)
-        {
-            perror("Socket creation failed");
-            exit(EXIT_FAILURE);
-        }
+// Initialize DPDK
 
-        sockaddr_in server_addr{};
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(port);
-        if (inet_pton(AF_INET, ip_address.c_str(), &server_addr.sin_addr) <= 0)
-        {
-            perror("Invalid address");
-            exit(EXIT_FAILURE);
-        }
-        if (bind(server_socket_, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-        {
-            perror("Binding failed");
-            exit(EXIT_FAILURE);
-        }
+	    mbuf_pool_ = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS, MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+	    if (!mbuf_pool_) {
+		std::cerr << "[ERROR] Failed to create DPDK memory pool!\n";
+		return;// -1;
+	    }
 
-        if (listen(server_socket_, 1) < 0)
-        { // Only one client
-            perror("Listen failed");
-            exit(EXIT_FAILURE);
-        }
-        char str[INET_ADDRSTRLEN];
-        std::cout << "Server connected:" << inet_ntop(AF_INET, &server_addr.sin_addr, str, INET_ADDRSTRLEN) << std::endl;
+	    if (rte_eth_dev_configure(portid, 1, 1, nullptr) < 0) {
+		std::cerr << "[ERROR] Failed to configure Ethernet device!\n";
+		return;// -1;
+	    }
+
+	    if (rte_eth_rx_queue_setup(portid, 0, RX_RING_SIZE, rte_eth_dev_socket_id(portid), nullptr, mbuf_pool_) < 0 ||
+		rte_eth_tx_queue_setup(portid, 0, TX_RING_SIZE, rte_eth_dev_socket_id(portid), nullptr) < 0) {
+		std::cerr << "[ERROR] Failed to set up RX/TX queues!\n";
+		return;// -1;
+	    }
+
+	    if (rte_eth_dev_start(portid) < 0) {
+		std::cerr << "[ERROR] Failed to start Ethernet device!\n";
+		return;// -1;
+	    }
+
+	    std::cout << "[INFO] DPDK UDP Server is running...\n";
+
         handler->initialize_buffers();
-        // stop_writer_thread_ = true;
-        client_socket_ = accept_client();
-        // close(server_socket_);
-        if (client_socket_ != -1)
+        if (!stop_reader_thread_)
         {
             std::cout << "Start reader and writer threads" << std::endl;
             reader_thread_ = std::thread(&DistMultServer::start_reading, this);
-            writer_thread_ = std::thread(&DistMultServer::writer_thread, this);
+            writer_thread_ = std::thread(&DistMultServer::writer_thread, this, ip_address, port);
         
 	    pin_thread_to_core(reader_thread_, 0);
 	    pin_thread_to_core(writer_thread_, 0);
@@ -90,9 +94,9 @@ public:
     }
 
 private:
-    int server_socket_;
-    int client_socket_;
+    struct rte_mempool *mbuf_pool_;
     bool stop_writer_thread_;
+    bool stop_reader_thread_;
     std::thread reader_thread_;
     std::thread writer_thread_;
     TaskHandler *task_handler_;
@@ -112,45 +116,14 @@ private:
         }
     }
     
-    int accept_client()
-    {
-        sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        int new_socket = accept(server_socket_, (struct sockaddr *)&client_addr, &client_len);
-        if (new_socket < 0)
-        {
-            perror("Accept failed");
-            return -1;
-        }
-        char str[INET_ADDRSTRLEN];
-        std::cout << "Client connected:" << inet_ntop(AF_INET, &client_addr.sin_addr, str, INET_ADDRSTRLEN) << std::endl;
-        return new_socket;
-    }
 
-    void start_reading()
-    {
-	while (!stop_writer_thread_){    
-	       struct pollfd pfd;
-	       pfd.fd = client_socket_;
-	       pfd.events = POLLIN; // Wait for incoming data
-		struct epoll_event event;
-		int epoll_fd = epoll_create1(0);
-		event.events = POLLIN;
-		event.data.fd = pfd.fd;
-		epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pfd.fd, &event);
+    void start_reading(){
+	struct rte_mbuf *bufs[BURST_SIZE];
+    	while(!stop_writer_thread_){
+		uint16_t nb_rx = rte_eth_rx_burst(portid, 0, bufs, BURST_SIZE);
+		if(nb_rx == 0){
+			//deal with closing socket here
 
-		struct epoll_event events[1];
-	      while (client_socket_ != -1)
-	      {
-		int ret = epoll_wait(epoll_fd, events, 1, 100);//int ret = poll(&pfd, 1, 100);
-		if (pfd.revents & POLLHUP || pfd.revents & POLLERR || pfd.revents & POLLNVAL) {
-		    std::cerr << "Socket closed by the peer! Attempting to reconnect..." << std::endl;
-		    return;
-		} else if (ret > 0/** && (pfd.revents & POLLIN)**/)
-		{
-		    uint32_t size;
-		    ssize_t bytes_received = recv(client_socket_, &size, sizeof(size), MSG_WAITALL);
-		    if (bytes_received == 0) {
 		      current_buffer_ = task_handler_->select_next_buffer();
                       int buffer_id = current_buffer_;// / 4;
                       int thread_id = 0;//current_buffer_;// % 4;
@@ -161,90 +134,116 @@ private:
  	              task_handler_->process_request(buffer_id, thread_id);
 
 		      stop_writer_thread_ = true;
-		      client_socket_ = -1;
+		      stop_reader_thread_ = true;
 		      return;
-		    } else if (bytes_received == -1) {
-			perror("recv() failed");
-			return;
-		   }
-		    size = ntohl(size);
-		    
-		    if (size == sizeof(int)){
-			int32_t result = 0;
-			recv(client_socket_, &result, size, MSG_WAITALL);
-			if (result == -1){
-			  	//anything here
-				return; 
-			}
-		    
-		    }
-		    current_buffer_ = task_handler_->select_next_buffer();
-		    int buffer_id = current_buffer_;// / 4;
-		    int thread_id = 0;//current_buffer_;// % 4;
-		    std::cout << "current buffer is: " << current_buffer_ << std::endl; 
-		    request_ = (MatrixRequest *)task_handler_->get_buffer_request(buffer_id, thread_id);
-		    std::string buffer(size, 0);
-		    recv(client_socket_, &buffer[0], size, MSG_WAITALL);
-
-		    if (!request_->ParseFromString(buffer))
-		    {
-			std::cerr << "Failed to parse protobuf message" << std::endl;
-			return;
-
-		    }
-		    else
-		    {
-			std::cout << "task_id is: " << request_->task_id() << std::endl;
-			task_handler_->process_request(buffer_id, thread_id);
-		    }
-		    break;
 		}
-		else if (ret == 0)
-		{
-		}
-		else
-		{
-		    break;
-		}
+		else if(nb_rx > 0){
+			for (uint16_t i = 0; i < nb_rx; i++){
+			   struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(bufs[i], struct rte_ether_hdr *);
+			   struct rte_ipv4_hdr *ip_hdr = (struct rte_ipv4_hdr *)(eth_hdr + 1);
+			   struct rte_udp_hdr *udp_hdr = (struct rte_udp_hdr *)(ip_hdr + 1);
+			   char *payload = (char *)(udp_hdr + 1);
+			   uint16_t data_len = ntohs(udp_hdr->dgram_len) - sizeof(struct rte_udp_hdr);
 
-	      }
+			   if(ip_hdr->next_proto_id == IPPROTO_UDP){
+			       current_buffer_ = task_handler_->select_next_buffer();
+			       int buffer_id = current_buffer_;// / 4;
+			       int thread_id = 0;//current_buffer_;// % 4;
+			       std::cout << "current buffer is: " << current_buffer_ << std::endl; 
+			       request_ = (MatrixRequest *)task_handler_->get_buffer_request(buffer_id, thread_id);
+				if (!request_->ParseFromArray(payload, data_len)){
+				    std::cerr << "Failed to parse protobuf message" << std::endl;
+				    return;
+
+			        }
+			        else
+			        {
+				    std::cout << "task_id is: " << request_->task_id() << std::endl;
+				    task_handler_->process_request(buffer_id, thread_id);
+			        }
+			        break;
+			    }
+					//process request
+			    std::cout << "Server received UDP packet" << std::endl;
+
+				// Echo back the received message
+			    rte_pktmbuf_free(bufs[i]);  // Free original packet
+			   }
+		} else {
+			//catch some error here!!
+		}
 	}
-    }
-
-    void writer_thread()
-    {
-        while (!stop_writer_thread_)
-        {
+}
+    
+void writer_thread(const std::string& ip_address, int dst_port){
+	while (!stop_writer_thread_){
+	
 	    int response_id = task_handler_->check_response();
             if (response_id == -1)
             {
                 LOG(INFO) << "OHNO Server writer: check response returns -1";
             }
-            else
-            {
+            else {
+
                 int buffer_id = response_id;// / 4;
                 int thread_id = 0;// response_id % 4;
 		std::cout << "buffer_id: " << buffer_id << " thread_id: " << thread_id << std::endl; 
 		response_ = (MatrixResponse *)task_handler_->get_buffer_response(buffer_id, thread_id);
-
+		
                 std::string serialized_response;
                 response_->SerializeToString(&serialized_response);
-                uint32_t size = htonl(serialized_response.size());
-                std::string final_message;                                                 // can we combine so this just uses one string
-                final_message.append(reinterpret_cast<const char *>(&size), sizeof(size)); // Prefix with size
-                final_message.append(serialized_response);                                 // Append protobuf data
+		
+		struct rte_mbuf *pkt = rte_pktmbuf_alloc(mbuf_pool_);
+		if (!pkt){
+			std::cerr << "Packet allocation failed";
+			return;
+		}
+		try {
+			struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(pkt, struct rte_ether_hdr *);
+			struct rte_ipv4_hdr *ip_hdr = (struct rte_ipv4_hdr*)(eth_hdr + 1);
+			struct rte_udp_hdr *udp_hdr = (struct rte_udp_hdr *)(ip_hdr + 1);
+			char *payload = (char *)(udp_hdr + 1);
 
-                if (client_socket_ != -1)
-                {
-                    if (send(client_socket_, final_message.c_str(), final_message.size(), 0) == -1)
-                    {
-                        perror("Failed to send response");
-                    }
-           	    task_handler_->add_resource(thread_id);
-                }
-            }
-        }
-    }
+			//Fill Ethernet header
+			rte_eth_macaddr_get(portid, &eth_hdr->src_addr);
+			memset(&eth_hdr->dst_addr, 0xFF, sizeof(eth_hdr->dst_addr));//Broadcast
+
+			eth_hdr->ether_type = htons(RTE_ETHER_TYPE_IPV4);
+			struct in_addr dst_ip;
+			inet_pton(AF_INET, ip_address.c_str(), &dst_ip);
+			//Fill IP header
+			ip_hdr->version_ihl = (4 << 4) | 5;
+			ip_hdr->total_length = htons(sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr) + serialized_response.size());
+			ip_hdr->next_proto_id = IPPROTO_UDP;
+			ip_hdr->src_addr = inet_addr("192.168.1.3");  // Server IP
+			ip_hdr->dst_addr = dst_ip.s_addr;
+
+			// Fill UDP header
+			udp_hdr->src_port = htons(5001);
+			udp_hdr->dst_port = htons(dst_port);
+			udp_hdr->dgram_len = htons(sizeof(struct rte_udp_hdr) + serialized_response.size());
+
+			//copy response data
+			memcpy(payload, &serialized_response, serialized_response.size());
+
+			//set packet lengths
+			pkt->data_len = sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr) + serialized_response.size();
+			pkt->pkt_len = pkt->data_len;
+
+			//send response
+			struct rte_mbuf *tx_pkts[1] = {pkt};
+			if (rte_eth_tx_burst(portid, 0, tx_pkts, 1) == 0) {
+				std::cerr << "[ERROR] Failed to send UDP response!\n";
+				rte_pktmbuf_free(pkt);
+			}
+			task_handler_->add_resource(thread_id);
+		}catch (const std::exception &e) {
+		        std::cerr << "[EXCEPTION] Error sending response: " << e.what() << "\n";
+        		rte_pktmbuf_free(pkt);
+		}	
+	    }
+	}
+}
 
     void stop()
     {
@@ -257,10 +256,6 @@ private:
         {
             writer_thread_.join();
         }
-        std::cerr << "Closing server socket at line " << __LINE__ << std::endl;
-        close(server_socket_);
-        std::cerr << "Closing client socket at line " << __LINE__ << std::endl;
-        close(client_socket_);
 	running = false;
     }
 
@@ -305,6 +300,11 @@ int main(int argc, char **argv)
         std::cerr << "Usage: " << argv[0] << " <task_type> <n> <<address>>\n";
         return EXIT_FAILURE;
     }
+
+    if (rte_eal_init(/**what arguments do I want here????**/argc, argv) < 0) {
+	std::cerr << "[ERROR] DPDK EAL initialization failed!\n";
+	return -1;
+     }
 
     std::string task_type = argv[1];
     std::string address = argv[3];
